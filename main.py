874b59e67a8f6d89df1ae0b4bb2e64bb6c7ce45a
Arun Kumar <arunsagi98@gmail.com>
2026-05-09 15:50:06 +0530

    Initial backend project

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Request body structure
+class LoginData(BaseModel):
+    email: str
+    password: str
+
+# Home API
+@app.get("/")
+def home():
+    return {
+        "message": "Backend Running Successfully"
+    }
+
+# Login API
+@app.post("/login")
+def login(data: LoginData):
+
+    if data.email == "admin@test.com" and data.password == "1234":
+
+        return {
+            "success": True,
+            "message": "Login Successful"
+        }
+
+    return {
+        "success": False,
+        "message": "Invalid Credentials"
+    }
